77d2226a1919fa1edb5deb1d499a5c7daaf3a235
juice <hugo.torres1993@gmail.com>
2020-03-21 13:58:11 -0400

    Initial commit
    Add basic generator for static files

---
+++ content/index.md
@@ -0,0 +1,8 @@
+# About Me
+
+I'm a Software Engineer based in South Florida.
+
+Currently I'm interested in site reliability, monitoring, logging, cloudy stuff like containerization. I also think networking is really cool.
+
+Outside of work I enjoy college football, gaming, working out and playing basketball. I also enjoy drinking wine and trying interesting food.
+
---
+++ gen/gen.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from markdown import markdown
+from yaml import load, FullLoader
+from sys import exit
+from os import mkdir
+from shutil import copytree
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+import codecs
+
+try:
+    mkdir('www')
+except FileExistsError:
+    print('www already exists, skipping build...')
+    exit(1)
+
+copytree('static/css', 'www/css')
+copytree('static/js', 'www/js')
+
+with open('gen.yml') as f:
+    config = load(f.read(), Loader=FullLoader)
+
+title = config['title']
+siteConfig = config['site']
+
+env = Environment(loader=FileSystemLoader('templates'), autoescape=select_autoescape([]))
+basetpl = env.get_template('base.tpl.html')
+
+for key in siteConfig:
+    pageConfig = siteConfig[key]
+
+    with codecs.open('content/{}'.format(pageConfig['source']), encoding='UTF-8') as input_file, \
+        codecs.open('www/{}'.format(key), 'w+', encoding='UTF-8') as output_file:
+
+        buf = input_file.read()
+        content = markdown(buf)
+        output_file.write(basetpl.render(title=title, content=content))

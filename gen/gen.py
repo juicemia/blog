41b2accdf004a81352e36fd16a4d048f071fb073
juice <hugo.torres1993@gmail.com>
2020-03-22 17:44:05 -0400

    Add generation of blog links in sidebar

--- gen/gen.py
+++ gen/gen.py
@@ -9,6 +9,8 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 import codecs
 
+from typ import BlogLink
+
 try:
     mkdir('www')
 except FileExistsError:
@@ -27,24 +29,29 @@ with open('gen.yml') as f:
     config = load(f.read(), Loader=FullLoader)
 
 title = config['title']
-siteConfig = config['site']
+site_config = config['site']
 
 env = Environment(loader=FileSystemLoader('templates'), autoescape=select_autoescape([]))
 basetpl = env.get_template('base.tpl.html')
 
-indexConfig = siteConfig['index.html']
-with codecs.open('content/{}'.format(indexConfig['source']), encoding='UTF-8') as input_file, \
+# Get every blog to be generated here
+blog_config = site_config['blog']
+blog_links = [BlogLink(blog_config[key]['preview'], '/blog/{}'.format(key)) for key in blog_config.keys()]
+
+env.globals['blog_links'] = blog_links
+
+index_config = site_config['index.html']
+with codecs.open('content/{}'.format(index_config['source']), encoding='UTF-8') as input_file, \
     codecs.open('www/index.html', 'w+', encoding='UTF-8') as output_file:
 
     buf = input_file.read()
     content = markdown(buf)
     output_file.write(basetpl.render(title=title, content=content))
 
-blogConfig = siteConfig['blog']
-for key in blogConfig:
-    pageConfig = blogConfig[key]
+for key in blog_config:
+    page_config = blog_config[key]
 
-    with codecs.open('content/blog/{}'.format(pageConfig['source']), encoding='UTF-8') as input_file, \
+    with codecs.open('content/blog/{}'.format(page_config['source']), encoding='UTF-8') as input_file, \
         codecs.open('www/blog/{}'.format(key), 'w+', encoding='UTF-8') as output_file:
 
         buf = input_file.read()
---
+++ gen/typ.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+class BlogLink():
+    def __init__(self, preview, href):
+        self.preview = preview
+        self.href = href
